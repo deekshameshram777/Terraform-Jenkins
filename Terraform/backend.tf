d4ee2e38b3667fd3f5364484f593828d8ef4b3f0
deekshameshram777 <156531844+deekshameshram777@users.noreply.github.com>
2024-06-20 23:23:22 +0530

    Delete Terraform directory

--- Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "test-rg"
-    storage_account_name = "teststorage001777"
-    container_name       = "devtest-tfstate"
-    key                  = "devtest.terraform.tfstate"
-  }
-}

acd571101731ebb8e28c09043053624cca673d71
deekshameshram777 <156531844+deekshameshram777@users.noreply.github.com>
2024-06-21 14:47:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "demorg" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "demostorage001" {
-  name                     = "${var.prefix}storage001"
+resource "azurerm_storage_account" "demostorage0011" {
+  name                     = "${var.prefix}storage0011"
   resource_group_name      = azurerm_resource_group.demorg.name
   location                 = var.location
   account_tier             = "Standard"
@@ -17,13 +17,13 @@ resource "azurerm_storage_account" "demostorage001" {
 
 resource "azurerm_storage_container" "democontainer" {
   name                  = "${var.prefix}-container"
-  storage_account_name  = azurerm_storage_account.demostorage001.name
+  storage_account_name  = azurerm_storage_account.demostorage0011.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "uploadfile" {
   name                   = "${var.prefix}file1"
-  storage_account_name   = azurerm_storage_account.demostorage001.name
+  storage_account_name   = azurerm_storage_account.demostorage0011.name
   storage_container_name = azurerm_storage_container.democontainer.name
   type                   = "Block"
   source                 = "providers.tf"
